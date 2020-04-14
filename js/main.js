c3848d87f0bbb9af6230ce5843df696b42745cda
bmurdock <brian.j.murdock@gmail.com>
2020-04-14 17:52:59 -0600

    First commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// predefine all of our DOM elements
+const cardMain = document.getElementById('cardMain');
+const pokemonName = document.getElementById('pokemonName');
+const pokemonImg = document.getElementById('pokemonImg');
+const typesBar = document.getElementById('typesBar');
+const heightElement = document.getElementById('heightElement');
+const weightElement = document.getElementById('weightElement');
+const refreshButton = document.getElementById('refreshButton');
